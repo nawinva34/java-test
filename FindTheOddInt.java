e888d9831baf28a8a657967476725ef0d350b181
Nawin <69630075+nawinva34@users.noreply.github.com>
2023-11-14 04:05:42 +0700

    Create FindTheOddInt.java

---
+++ FindTheOddInt.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class FindTheOddInt {
+
+    public static int findOddOccurrence(int[] array) {
+        Map<Integer, Integer> occurrenceMap = new HashMap<>();
+
+        for (int num : array) {
+            occurrenceMap.put(num, occurrenceMap.getOrDefault(num, 0) + 1);
+        }
+
+        for (int num : occurrenceMap.keySet()) {
+            if (occurrenceMap.get(num) % 2 != 0) {
+                return num;
+            }
+        }
+
+        return Integer.MIN_VALUE;
+    }
+
+    public static void main(String[] args) {
+        // Example usage
+        System.out.println(findOddOccurrence(new int[]{7}));                                     // Output: 7
+        System.out.println(findOddOccurrence(new int[]{0}));                                     // Output: 0
+        System.out.println(findOddOccurrence(new int[]{1, 1, 2}));                               // Output: 2
+        System.out.println(findOddOccurrence(new int[]{0, 1, 0, 1, 0}));                         // Output: 0
+        System.out.println(findOddOccurrence(new int[]{1, 2, 2, 3, 3, 3, 4, 3, 3, 3, 2, 2, 1})); // Output: 4
+        System.out.println(findOddOccurrence(new int[]{1, 1, 2, 1, 3, 2})); 
+    }
+}
